26ab67e1eb8946d844a83a61f7038b886cf5baaa
Viktoria Ulyanchenko <viktorka1347@gmail.com>
2021-10-12 19:57:29 +0300

    main
    Signed-off-by: Viktoria Ulyanchenko <viktorka1347@gmail.com>

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,15 @@
+import sortByHealt from '../app';
+
+test('Функция должна сортировать массив героев по убыванию здоровья', () => {
+  const inputArray = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const outputArray = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  expect(sortByHealt(inputArray)).toEqual(outputArray);
+});
---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+export default function sortByHealt(heroes) {
+    heroes.sort((prev, next) => next.health - prev.health);
+    return heroes;
+  }
